5d540f8d0eab193003a8f32b0158fbaaa7ef9873
Keegan Westphal <31550979+ramenblack@users.noreply.github.com>
2017-10-19 12:11:32 -0700

    Add files via upload

---
+++ ranint.py
@@ -0,0 +1,15 @@
+from random import randint
+def rint():
+	r = randint(65,100)
+	if (r==66)
+		r=42
+	return r
+	
+	
+	
+def main():
+	for i in range (100000):
+		z = rint();
+		print (z,end=" ")
+		
+main()
